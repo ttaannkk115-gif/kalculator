e542a6006717738bef74c5021c5d08eeb82d2644
ttaannkk115-gif <ttaannkk115@gmail.com>
2026-03-02 18:31:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
+// sw.js
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.json() : { title: 'VOLT MASTER', body: 'Новое событие!' };
-    event.waitUntil(self.registration.showNotification(data.title, {
+    const data = event.data ? event.data.json() : { title: 'VoltMaster', body: 'Новая шабашка!' };
+    
+    const options = {
         body: data.body,
-        icon: 'logo.png'
-    }));
+        icon: 'logo.png', // Путь к вашей аватарке
+        badge: 'logo.png',
+        vibrate: [200, 100, 200]
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+// Открытие приложения при клике на уведомление
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
 });
