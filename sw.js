2c101fe2b638264003b6be61907e060aa8db4ed0
ttaannkk115-gif <ttaannkk115@gmail.com>
2026-03-02 20:52:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
+const CACHE_NAME = 'voltmaster-v1';
+// Список файлов для офлайн-доступа
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './logo.png'
+];
+
+// Установка и кэширование
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// ОБЯЗАТЕЛЬНО для установки PWA: обработка запросов
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
+
+// Твои уведомления
 self.addEventListener('push', function(event) {
     const options = {
         body: 'Появилась новая работа!',
