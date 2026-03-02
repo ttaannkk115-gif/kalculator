51e805bb563acdbcbcdaf43102cf181b504838af
ttaannkk115-gif <ttaannkk115@gmail.com>
2026-03-03 00:58:14 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,22 @@
-importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-// Твои настройки VoltMaster
 firebase.initializeApp({
-  apiKey: "AIzaSyD-dIC8bm8y4kaY7RXmMrys2wOMLA5o8vY",
-  authDomain: "voltmaster-332c0.firebaseapp.com",
-  databaseURL: "https://voltmaster-332c0-default-rtdb.firebaseio.com",
-  projectId: "voltmaster-332c0",
-  storageBucket: "voltmaster-332c0.firebasestorage.app",
-  messagingSenderId: "312337098429",
-  appId: "1:312337098429:web:663e0f6787bd62d19bf9d4"
+    apiKey: "AIzaSyD-dIC8bm8y4kaY7RXmMrys2wOMLA5o8vY",
+    databaseURL: "https://voltmaster-332c0-default-rtdb.firebaseio.com",
+    projectId: "voltmaster-332c0",
+    messagingSenderId: "312337098429",
+    appId: "1:312337098429:web:663e0f6787bd62d19bf9d4"
 });
 
 const messaging = firebase.messaging();
 
-// Этот код ловит уведомление в фоновом режиме
+// Логика отображения уведомлений, когда приложение закрыто
 messaging.onBackgroundMessage((payload) => {
-  console.log('Получено фоновое сообщение:', payload);
-  
-  const notificationTitle = payload.notification.title || "VoltMaster";
-  const notificationOptions = {
-    body: payload.notification.body || "У вас новое уведомление!",
-    icon: 'logo.png',
-    badge: 'logo.png',
-    vibrate: [200, 100, 200]
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: 'logo.png'
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
