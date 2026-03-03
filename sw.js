acdb3a8b50357141fabe33ac9dd2e26a50a10e97
ttaannkk115-gif <ttaannkk115@gmail.com>
2026-03-04 02:10:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,38 @@
+importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD-dIC8bm8y4kaY7RXmMrys2wOMLA5o8vY",
+    databaseURL: "https://voltmaster-332c0-default-rtdb.firebaseio.com",
+    projectId: "voltmaster-332c0",
+    messagingSenderId: "312337098429",
+    appId: "1:312337098429:web:663e0f6787bd62d19bf9d4"
+});
+
+const messaging = firebase.messaging();
+
+// Фоновое уведомление
+messaging.onBackgroundMessage((payload) => {
+    const title = payload.notification.title;
+    const options = {
+        body: payload.notification.body,
+        icon: 'logo.png',
+        data: { url: payload.data.url } // Передаем ссылку в клик
+    };
+    return self.registration.showNotification(title, options);
+});
+
+// ОБРАБОТКА КЛИКА
 self.addEventListener('notificationclick', (event) => {
     event.notification.close();
-    
-    // Берем ссылку из данных пуша или открываем главную по умолчанию
-    const targetUrl = event.notification.data && event.notification.data.url 
-                      ? event.notification.data.url 
-                      : 'index.html';
+    const targetUrl = event.notification.data.url;
 
     event.waitUntil(
         clients.matchAll({ type: 'window' }).then((windowClients) => {
-            // Если вкладка уже открыта, переходим в неё
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                if (client.url.includes(targetUrl) && 'focus' in client) {
-                    return client.focus();
-                }
+            for (let client of windowClients) {
+                if (client.url === targetUrl && 'focus' in client) return client.focus();
             }
-            // Если нет — открываем новую
-            if (clients.openWindow) {
-                return clients.openWindow(targetUrl);
-            }
+            if (clients.openWindow) return clients.openWindow(targetUrl);
         })
     );
 });
